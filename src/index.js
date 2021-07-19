68e405b26633b1dbc2cf0e240780a3d77f2a4288
Chris Glesmann <chrisglesmann@gmail.com>
2021-07-18 19:36:20 -0700

    Removed Unneeded Modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 
-const teamRosterSetup = require("./modules/rosterSetup.js");
-const gameSetup = require("./modules/gameSetup.js");
 const teamBuilder = require("./modules/teamBuilder.js");
 
 const { Client } = require("discord.js");
@@ -21,7 +19,7 @@ botClient.on('message', (message) =>
         if (receivedCommandName === "generateTeam")
         {
             let amountOfAI = args[0];
-            let teamRosterConfig = args[1]?.split(",");
+            let teamRosterConfig = args[1]?.split(",")?.map((value) => { return Number(value); });
 
             if (!amountOfAI || (!teamRosterConfig || teamRosterConfig.length === 0))
             {
@@ -35,50 +33,7 @@ botClient.on('message', (message) =>
 
             message.channel.send(teamBuilder.run(teamRosterConfig, gameConfig));
         }
-        /*
-        let newTeam = await main();
-
-        console.log(newTeam);
-        message.channel.send(newTeam);
-        */
     }
 });
 
 botClient.login(process.env.DISCORDJS_BOT_TOKEN);
-
-/*
-async function main()
-{
-    printSectionHeader("Team Roster Config");
-    let desiredTeamRosterConfig = await teamRosterSetup.run();
-
-    printSectionHeader("Game Config");
-    let gameConfig = await gameSetup.run();
-
-    printSectionHeader("Generated Teams");
-    return teamBuilder.run(desiredTeamRosterConfig, gameConfig);
-}
-
-function printSectionHeader(headerString)
-{
-    console.log("\n-----------------------------------");
-    console.log(`${headerString}`);
-    console.log("-----------------------------------");
-}
-
-async function callMain()
-{
-    let displayValue;
-    try
-    {
-        displayValue = await main();
-    } catch (e)
-    {
-        console.error(`Error: ${e.message}`);
-    }
-
-    console.log(displayValue);
-}
-
-callMain();
-*/
